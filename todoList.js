bb103c8cdf67cb746c0db102adc68c8bd0f5d3c6
hoangnv <nvhoang2012002@gmail.com>
2024-01-13 21:50:58 +0700

    second commit

---
+++ todoList.js
@@ -0,0 +1,56 @@
+const input = document.querySelector("input");
+const addBtn = document.getElementById("add-btn");
+const list = document.getElementById("list");
+let listItem = JSON.parse(localStorage.getItem("listItem")) || []
+
+document.addEventListener("DOMContentLoaded", () => {
+    firstRender(listItem)
+})
+
+function setLocalStorage() {
+    localStorage.setItem('listItem', JSON.stringify(listItem))
+}
+
+// CREATING LI & DEL BTN
+function firstRender(listItem) {
+    list.innerHTML = ""
+    listItem.forEach(item => {
+        const todo = document.createElement('li');
+        todo.id = item.id;
+        todo.innerHTML = ` 
+            ${item.content}
+            <button onClick="deleteBtn(${item.id})">x</button>
+        `;
+        list.appendChild(todo);
+    });
+
+};
+
+// DELETE ITEM FUNCTION
+function deleteBtn(id) {
+    listItem = listItem.filter(item => item.id !== id);
+    setLocalStorage()
+    firstRender(listItem)
+}
+
+// ADD FUNCTION
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (input.value === "") {
+        window.alert("please, add your tasks!");
+        return;
+    };
+
+    const item = {
+        id: Math.floor(Math.random() * 1000),
+        content: input.value
+    };
+
+    listItem = [...listItem, item];
+    firstRender(listItem);
+
+    setLocalStorage();
+
+    input.value = "";
+})
